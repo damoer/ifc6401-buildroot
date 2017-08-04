999e5c62f17a3498e8263027425c7e35b242574c
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2017-08-04 18:59:52 +0200

    wpe/dyz: build new WPE launcher

---
+++ package/wpe/dyz/dyz.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# dyz
+#
+################################################################################
+
+DYZ_VERSION = ec7a8b93e2c6afb04a7f7feb76d28bfddffaaea0
+DYZ_SITE = $(call github,Igalia,dyz,$(DYZ_VERSION))
+
+DYZ_INSTALL_STAGING = YES
+DYZ_DEPENDENCIES = luajit
+
+DYZ_MAKE_ENV = \
+	$(TARGET_MAKE_ENV) \
+	CC=$(TARGET_CC) \
+	PKG_CONFIG="$(HOST_DIR)/bin/pkg-config" \
+	PKG_CONFIG_PATH="$(TARGET_DIR)/usr/lib/pkgconfig"
+
+$(eval $(autotools-package))
